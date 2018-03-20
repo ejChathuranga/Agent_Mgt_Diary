ece110b633d34a9309a7a0051f8fdbde834a5f07
ejChathuranga <ejcindunil@gmail.com>
2018-03-20 05:54:19 +0530

    Initial commit

---
+++ app/src/androidTest/java/genesip/com/ej/insuarance_agent_mgt_diary/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package genesip.com.ej.insuarance_agent_mgt_diary;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("genesip.com.ej.insuarance_agent_mgt_diary", appContext.getPackageName());
+    }
+}
