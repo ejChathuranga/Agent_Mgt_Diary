be76236cf3ffea11be0b7e3405017e4d43aabae9
EJ Chathuranga <ejcindunil@gmail.com>
2018-03-22 22:35:49 +0530

    adding DbConfig class

--- app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/db/DbConfig.java
+++ app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/db/DbConfig.java
@@ -4,20 +4,30 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
+import genesip.com.ej.insuarance_agent_mgt_diary.db.TableConfig.*;
+
 /**
  * Created by ej on 3/22/2018.
  */
 
-public class dbConfig extends SQLiteOpenHelper {
+public class DbConfig extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "AgentDiary_v1";
     public static final int DB_V = 1;
 
     /**
      * @param context
+     * let's define queries for create each table
      */
 
-    public dbConfig(Context context) {
+    public static final String SQL_CREATE_AGENT = "CREATE TABLE "+ Agent.TABLE_NAME+" (\n" +
+            "\t"+Agent._ID+"\tINTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t"+Agent.C_FIRSTNAME+"\tTEXT,\n" +
+            ");";
+
+
+    public DbConfig(Context context) {
         super(context, DB_NAME, null, DB_V);
     }
 
