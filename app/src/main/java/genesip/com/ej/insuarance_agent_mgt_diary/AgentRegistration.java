6093a5f73df7b7a3e1aee7347d22122a88a27aff
EJ Chathuranga <ejcindunil@gmail.com>
2018-03-26 05:36:43 +0530

    added neededd variables

--- app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/AgentRegistration.java
+++ app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/AgentRegistration.java
@@ -1,10 +1,26 @@
 package genesip.com.ej.insuarance_agent_mgt_diary;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import genesip.com.ej.insuarance_agent_mgt_diary.db.DbConfig;
 
 public class AgentRegistration extends AppCompatActivity {
 
+    private DbConfig dbConfig;
+
+    private EditText firstNamr, lastName, username, pass, mobileNo, mail, ansewer ;
+    private Spinner role, secQue;
+    private Button reg;
+
+    private SQLiteDatabase mDatabase;
     @Override
     public boolean onSupportNavigateUp() {
         finish();
