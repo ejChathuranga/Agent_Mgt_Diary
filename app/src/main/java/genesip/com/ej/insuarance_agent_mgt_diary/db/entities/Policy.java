bc2acab5fc5af6534680bfafc478b63149dcff47
EJ Chathuranga <ejcindunil@gmail.com>
2018-03-27 22:13:53 +0530

    for each table created classes for set attribute names
    removed static concept. So no need to put table config class

---
+++ app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/db/entities/Policy.java
@@ -0,0 +1,10 @@
+package genesip.com.ej.insuarance_agent_mgt_diary.db.entities;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ej on 3/27/2018.
+ */
+
+public class Policy implements BaseColumns {
+}
