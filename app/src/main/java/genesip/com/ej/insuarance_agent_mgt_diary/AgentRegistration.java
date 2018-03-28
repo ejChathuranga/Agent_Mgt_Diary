f3cb9514768b762a5aaa2730630b357c9cfd6b48
EJ Chathuranga <ejcindunil@gmail.com>
2018-03-28 07:51:21 +0530

    implement Async class to send values to save db

--- app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/AgentRegistration.java
+++ app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/AgentRegistration.java
@@ -1,8 +1,10 @@
 package genesip.com.ej.insuarance_agent_mgt_diary;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,10 +15,13 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import genesip.com.ej.insuarance_agent_mgt_diary.db.DbActivites;
 import genesip.com.ej.insuarance_agent_mgt_diary.db.DbConfig;
+import genesip.com.ej.insuarance_agent_mgt_diary.db.entities.Agent;
 
 public class AgentRegistration extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "AGENT REGISTRATION";
     private DbConfig dbConfig;
 
     private EditText firstName, lastName, username, pass, mobileNo, email, answer ;
@@ -61,9 +66,10 @@ public class AgentRegistration extends AppCompatActivity implements View.OnClick
         switch (v.getId()){
             case (R.id.btnReg): {
 
-
-
-                Toast.makeText(this, "Woohhhaaaa", Toast.LENGTH_SHORT).show();
+                new AgentRegAsync().execute();
+//                DbActivites obj = DbActivites.getInstance();
+//                obj.showMassge();
+//                Toast.makeText(this, "Woohhhaaaa", Toast.LENGTH_SHORT).show();
                 break;
             }
         }
@@ -88,6 +94,24 @@ public class AgentRegistration extends AppCompatActivity implements View.OnClick
             String emailA = email.getText().toString();
             String secQueA = secQue.getSelectedItem().toString();
             String answerA = answer.getText().toString();
+
+            ContentValues values = new ContentValues();
+            Agent agent  = new Agent();
+            DbActivites dbActivites = DbActivites.getInstance();
+
+            values.put(agent.getC_FIRSTNAME(), firstNameA);
+            values.put(agent.getC_LASTNAME(), lastNameA);
+            values.put(agent.getC_USERNAME(), usernameA);
+            values.put(agent.getC_PASS(), passA);
+            values.put(agent.getC_MOBILE(), mobileNoA);
+            values.put(agent.getC_ROLE(), roleA);
+            values.put(agent.getC_EMAIL(), emailA);
+            values.put(agent.getC_SEC_QUE(), secQueA);
+            values.put(agent.getC_ANSWER(), answerA);
+
+            String response = dbActivites.saveIntoDB(values, getApplicationContext(), "Agent");
+//            Toast.makeText(AgentRegistration.this, response, Toast.LENGTH_SHORT).show();
+            Log.e(TAG,response);
 
 
             return null;
