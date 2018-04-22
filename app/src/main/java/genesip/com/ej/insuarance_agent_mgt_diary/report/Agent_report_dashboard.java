f3bc002f63469e54819c629c701e53e3554cae40
EJ Chathuranga <ejcindunil@gmail.com>
2018-04-23 03:36:40 +0530

    added drawble files for show dump charts

---
+++ app/src/main/java/genesip/com/ej/insuarance_agent_mgt_diary/report/Agent_report_dashboard.java
@@ -0,0 +1,13 @@
+package genesip.com.ej.insuarance_agent_mgt_diary.report;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Agent_report_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agent_report_dashboard);
+    }
+}
